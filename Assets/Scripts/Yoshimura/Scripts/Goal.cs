f56960d7f35a467d1ae15a1b7747422442fd6437
DESKTOP-EQEV5OA\PC_User <yoshi1828yoc@gmail.com>
2022-10-21 18:35:39 +0900

    Goal判定の作成

---
+++ Assets/Scripts/Yoshimura/Scripts/Goal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    //プレイヤーのオブジェクト
+    public GameObject BlueHakoberos;
+   
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // Debug.Log("hit");
+
+        //もしゴールオブジェクトのコライダーに接触した時の処理。
+        if (other.name == BlueHakoberos.name)
+        {
+           
+                //キャラクターを非表示にする処理
+                BlueHakoberos.SetActive(false);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Yoshimura/Scripts/TestPlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+
+    //テストなので、後で消します
+
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+    }
+}
