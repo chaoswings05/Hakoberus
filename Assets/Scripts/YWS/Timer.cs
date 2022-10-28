e4b88abe4c7e2f54ecd6a6f8bb05e6a5b24074d5
chaoswings05 <83049802+chaoswings05@users.noreply.github.com>
2022-10-28 18:32:16 +0900

    時間経過でSkyboxの色が変わる機能

--- Assets/Scripts/YWS/Timer.cs
+++ Assets/Scripts/YWS/Timer.cs
@@ -5,16 +5,20 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    private int phase = 0;
     private float elapsedTime = 0.0f;
-    [Header("制限時間")]public float timeLimit = 300f;
-    [Header("時間に応じて変化するオブジェクト"), SerializeField] private Image controlObj = null;
+    private Color initialSkyTint = new Color(128/255f, 128/255f, 128/255f);
+    [Header("制限時間")] private float timeLimit = 300f;
+    [SerializeField] private AnimationCurve Curve_R, Curve_G, Curve_B;
+    [SerializeField] private Text display = null;
 
     // Start is called before the first frame update
     void Start()
     {
         //初期化
+        phase = 0;
         elapsedTime = 0.0f;
-        controlObj.color = Color.cyan;
+        RenderSettings.skybox.SetColor("_SkyTint", initialSkyTint);
     }
 
     // Update is called once per frame
@@ -23,16 +27,28 @@ public class Timer : MonoBehaviour
         if (elapsedTime < timeLimit)
         {
             elapsedTime += Time.deltaTime;
-            Debug.Log(elapsedTime);
+
+            if (phase != 1 && elapsedTime >= 60f && elapsedTime <= 180f)
+            {
+                phase = 1;
+                RenderSettings.skybox.SetColor("_SkyTint", new Color (253/255f, 126/255f, 0f));
+                Debug.Log("Phase 0 to Phase 1");
+            }
+            else if (phase != 2 && elapsedTime >= 180f)
+            {
+                phase = 2;
+                RenderSettings.skybox.SetColor("_SkyTint", new Color(0f, 103/255f, 192/255f));
+                Debug.Log("Phase 1 to Phase 2");
+            }
+
+            Color col = new Color(Curve_R.Evaluate(elapsedTime), Curve_G.Evaluate(elapsedTime), Curve_B.Evaluate(elapsedTime));
+            RenderSettings.skybox.SetColor("_SkyTint", col);
+        }
+        else if (elapsedTime >= timeLimit)
+        {
+            Debug.Log("Game End");
         }
 
-        if (elapsedTime >= 60f && elapsedTime <= 180f)
-        {
-            controlObj.color = Color.yellow;
-        }
-        else if (elapsedTime >= 180f)
-        {
-            controlObj.color = Color.blue;
-        }
+        display.text = elapsedTime.ToString();
     }
 }
