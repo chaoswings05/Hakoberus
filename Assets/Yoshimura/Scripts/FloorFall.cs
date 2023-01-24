52cb8d6bdc7d11cc0f6805ff7968a47090628eb4
DESKTOP-EQEV5OA\PC_User <yoshi1828yoc@gmail.com>
2023-01-25 00:51:59 +0900

    ステージ２だいたい完成

--- Assets/Yoshimura/Scripts/FloorFall.cs
+++ Assets/Yoshimura/Scripts/FloorFall.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class FloorFall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
-    // Update is called once per frame
+    [SerializeField]
+    int count;
+
+    //乗れるハコベロスの数を設定してください
+    [SerializeField]
+    int fallcount;
+
+
     void Update()
     {
         
+        if(fallcount==count)
+        {
+           
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //PlayerかSubタグにあったらカウントを増やす
+        //Subタグは赤ハコベロスのこと
+        if (other.tag == "Player" || other.tag == "Sub")
+        {
+            count++;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //PlayerかSubタグが離れたらカウントを減らす
+        if (other.tag == "Player" || other.tag == "Sub")
+        {
+            count--;
+           
+        }
     }
 }
