f2ec4c26292f0f01e56dc8a20be695357a076b00
chaoswings05 <83049802+chaoswings05@users.noreply.github.com>
2023-02-03 18:25:54 +0900

    チュートリアルウインドウ調整途中

--- Assets/YWS/GameDirector.cs
+++ Assets/YWS/GameDirector.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameDirector : MonoBehaviour
 {
@@ -25,15 +27,34 @@ public class GameDirector : MonoBehaviour
 
     [SerializeField, Header("流すBGMの番号")] private int BGMNum = 0;
     public ActionArea AP = null;
+    [SerializeField, Header("ウインドウの背景画像")] private Image window = null;
+    [SerializeField, Header("ウインドウのテキスト")] private Text text = null;
+    public bool IsGameStart = false;
+    [SerializeField, Header("ゲームが開始するまでの時間")] private float StartTime = 8f;
+    private float elapsedTime = 0f;
+    private bool IsFadeStart = false;
+    [SerializeField, Header("ウインドウが消えるまでの時間")] private float fadeTime = 1f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         SoundManager.Instance.PlayBGM(BGMNum);
+        IsGameStart = false;
     }
 
     void Update()
     {
-        
+        if (!IsFadeStart)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime >= StartTime)
+            {
+                elapsedTime = 0f;
+                text.DOFade(0f, fadeTime);
+                window.DOFade(0f, fadeTime).OnComplete(()=> IsGameStart = true);
+                IsFadeStart = true;
+            }
+        }
     }
 }
